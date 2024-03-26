8235a68197b2d239d9cd61b069a2005cf085a7ff
sliman2 <161692272+sliman2@users.noreply.github.com>
2024-03-26 16:17:24 +0100

    Update registry.ts

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -20,6 +20,20 @@ export async function launchRegistry() {
 
   // TODO implement the status route
   // _registry.get("/status", (req, res) => {});
+    _registry.get("/status", (req: Request, res: Response) => {
+    res.send("live");
+  });
+
+  const nodes: Node[] = [];
+  _registry.post("/registerNode", (req: Request, res: Response) => {
+    const { nodeId, pubKey }: RegisterNodeBody = req.body;
+    nodes.push({ nodeId, pubKey });
+    res.json({ result: "success" });
+  });
+
+  _registry.get("/getNodeRegistry", (req: Request, res: Response) => {
+    res.json({ nodes });
+  });
 
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
